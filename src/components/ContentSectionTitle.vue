bc3485b12de3144523dbd3da69c7ab29615a4fe4
Zoe Curry <zoe.curry@wra.gov.wales>
2024-02-22 13:41:40 +0000

    add content section title

---
+++ src/components/ContentSectionTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="content-section-title">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "wra-content-section-title"
+};
+</script>
+
+<style scoped>
+.content-section-title {
+  border-bottom: 2px solid #e5e5e5;
+  border-top: 10px solid #e5e5e5;
+  padding-bottom: 20px;
+  padding-top: 30px;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import ValidationTooltip from "./components/ValidationTooltip.vue";
 import Checkbox from "./components/Checkbox.vue";
 import CheckboxGroup from "./components/CheckboxGroup.vue";
+import ContentSectionTitle from "./components/ContentSectionTitle.vue";
 
 const components = {
   ValidationTooltip,
   Checkbox,
-  CheckboxGroup
+  CheckboxGroup,
+  ContentSectionTitle
 };
 
 export default (app) => {
