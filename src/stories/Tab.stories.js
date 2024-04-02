c2ac566fb2e34f8df8bd09cd9c9e60ffb9d18551
Jake <jake.real@wra.gov.wales>
2024-04-02 16:16:44 +0100

    🚧 work in progress: tab and tabs

---
+++ src/stories/Tab.stories.js
@@ -0,0 +1,14 @@
+import Tab from "../components/Tab.vue";
+
+export default {
+  title: "All/Tab",
+  component: Tab,
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    label: "This is a single tab",
+    active: false,
+  },
+};
