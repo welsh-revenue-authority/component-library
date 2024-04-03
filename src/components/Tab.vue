c97dfd368cd96ff6146c205b4aef31085725c6a5
Jake <jake.real@wra.gov.wales>
2024-04-03 15:05:28 +0100

    💄 update style: fix borders, add focus state

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -28,13 +28,21 @@ export default {
 </script>
 
 <style scoped>
-.tab-button {
-  font-weight: bold;
+/* Reset */
+* {
+  box-sizing: border-box;
   border: none;
-  padding: 16px;
-  font-size: 20px;
+}
+
+.tab-button {
+  border-top: 6px #0360a6 solid;
+  font-size: 24px;
+  font-weight: bold;
   color: #c2e0fc;
   background: #1f446d;
+  padding: 20px;
+  margin-left: 15px;
+  margin-top: 40px;
 }
 
 .tab-button:hover {
@@ -45,10 +53,27 @@ export default {
 .tab-button--active {
   background: #ffffff;
   color: #1f1f1f;
+  border-top: 6px #cccccc solid;
 }
 
 .tab-button--active:hover {
   background: #e5e5e5;
   color: #1f1f1f;
+}
+
+.tab-button:focus {
+  border-top: 6px #0360a6 solid;
+  color: #1f1f1f;
+  background: #ffd530;
+  box-shadow: 2px 2px 0px 0px #1f1f1f inset, -2px -2px 0px 0px #1f1f1f inset;
+}
+
+@media (max-width: 768px) {
+  .tab-button {
+    margin-left: 10px;
+    margin-top: 20px;
+    font-size: 20px;
+    padding: 15px;
+  }
 }
 </style>
