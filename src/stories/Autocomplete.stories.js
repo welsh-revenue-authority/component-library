002fce4a6ebc9ec9b9adbe3cf85f8fbea3e4c528
Jake <jake.real@wra.gov.wales>
2024-04-03 16:48:34 +0100

    ♻️ refactor: remove duplication of councils in story

--- src/stories/Autocomplete.stories.js
+++ src/stories/Autocomplete.stories.js
@@ -20,100 +20,102 @@ export default {
   tags: ["autodocs"],
 };
 
+let councilArray = [
+  {
+    value: "Blaenau Gwent",
+    label: "Blaenau Gwent",
+  },
+  {
+    value: "Bridgend",
+    label: "Bridgend",
+  },
+  {
+    value: "Caerphilly",
+    label: "Caerphilly",
+  },
+  {
+    value: "Cardiff",
+    label: "Cardiff",
+  },
+  {
+    value: "Carmarthenshire",
+    label: "Carmarthenshire",
+  },
+  {
+    value: "Ceredigion",
+    label: "Ceredigion",
+  },
+  {
+    value: "Conwy",
+    label: "Conwy",
+  },
+  {
+    value: "Denbighshire",
+    label: "Denbighshire",
+  },
+  {
+    value: "Flintshire",
+    label: "Flintshire",
+  },
+  {
+    value: "Gwynedd",
+    label: "Gwynedd",
+  },
+  {
+    value: "Isle of Anglesey",
+    label: "Isle of Anglesey",
+  },
+  {
+    value: "Merthyr Tydfil",
+    label: "Merthyr Tydfil",
+  },
+  {
+    value: "Monmouthshire",
+    label: "Monmouthshire",
+  },
+  {
+    value: "Neath Port Talbot",
+    label: "Neath Port Talbot",
+  },
+  {
+    value: "Newport",
+    label: "Newport",
+  },
+  {
+    value: "Pembrokeshire",
+    label: "Pembrokeshire",
+  },
+  {
+    value: "Powys",
+    label: "Powys",
+  },
+  {
+    value: "Rhondda Cynon Taf",
+    label: "Rhondda Cynon Taf",
+  },
+  {
+    value: "Swansea",
+    label: "Swansea",
+  },
+  {
+    value: "Torfaen",
+    label: "Torfaen",
+  },
+  {
+    value: "Vale of Glamorgan",
+    label: "Vale of Glamorgan",
+  },
+  {
+    value: "Wrexham",
+    label: "Wrexham",
+  },
+];
+
 export const Default = {
   args: {
     label: "This is an autocomplete",
     id: "test-autocomplete",
-    options: [
-      {
-        value: "Blaenau Gwent",
-        label: "Blaenau Gwent",
-      },
-      {
-        value: "Bridgend",
-        label: "Bridgend",
-      },
-      {
-        value: "Caerphilly",
-        label: "Caerphilly",
-      },
-      {
-        value: "Cardiff",
-        label: "Cardiff",
-      },
-      {
-        value: "Carmarthenshire",
-        label: "Carmarthenshire",
-      },
-      {
-        value: "Ceredigion",
-        label: "Ceredigion",
-      },
-      {
-        value: "Conwy",
-        label: "Conwy",
-      },
-      {
-        value: "Denbighshire",
-        label: "Denbighshire",
-      },
-      {
-        value: "Flintshire",
-        label: "Flintshire",
-      },
-      {
-        value: "Gwynedd",
-        label: "Gwynedd",
-      },
-      {
-        value: "Isle of Anglesey",
-        label: "Isle of Anglesey",
-      },
-      {
-        value: "Merthyr Tydfil",
-        label: "Merthyr Tydfil",
-      },
-      {
-        value: "Monmouthshire",
-        label: "Monmouthshire",
-      },
-      {
-        value: "Neath Port Talbot",
-        label: "Neath Port Talbot",
-      },
-      {
-        value: "Newport",
-        label: "Newport",
-      },
-      {
-        value: "Pembrokeshire",
-        label: "Pembrokeshire",
-      },
-      {
-        value: "Powys",
-        label: "Powys",
-      },
-      {
-        value: "Rhondda Cynon Taf",
-        label: "Rhondda Cynon Taf",
-      },
-      {
-        value: "Swansea",
-        label: "Swansea",
-      },
-      {
-        value: "Torfaen",
-        label: "Torfaen",
-      },
-      {
-        value: "Vale of Glamorgan",
-        label: "Vale of Glamorgan",
-      },
-      {
-        value: "Wrexham",
-        label: "Wrexham",
-      },
-    ],
+    options: councilArray,
   },
 };
 
@@ -122,95 +124,6 @@ export const Autoexpand = {
     label: "This is an autocomplete that expands when clicked",
     id: "test-autocomplete-autoexpand",
     autoExpand: true,
-    options: [
-      {
-        value: "Blaenau Gwent",
-        label: "Blaenau Gwent",
-      },
-      {
-        value: "Bridgend",
-        label: "Bridgend",
-      },
-      {
-        value: "Caerphilly",
-        label: "Caerphilly",
-      },
-      {
-        value: "Cardiff",
-        label: "Cardiff",
-      },
-      {
-        value: "Carmarthenshire",
-        label: "Carmarthenshire",
-      },
-      {
-        value: "Ceredigion",
-        label: "Ceredigion",
-      },
-      {
-        value: "Conwy",
-        label: "Conwy",
-      },
-      {
-        value: "Denbighshire",
-        label: "Denbighshire",
-      },
-      {
-        value: "Flintshire",
-        label: "Flintshire",
-      },
-      {
-        value: "Gwynedd",
-        label: "Gwynedd",
-      },
-      {
-        value: "Isle of Anglesey",
-        label: "Isle of Anglesey",
-      },
-      {
-        value: "Merthyr Tydfil",
-        label: "Merthyr Tydfil",
-      },
-      {
-        value: "Monmouthshire",
-        label: "Monmouthshire",
-      },
-      {
-        value: "Neath Port Talbot",
-        label: "Neath Port Talbot",
-      },
-      {
-        value: "Newport",
-        label: "Newport",
-      },
-      {
-        value: "Pembrokeshire",
-        label: "Pembrokeshire",
-      },
-      {
-        value: "Powys",
-        label: "Powys",
-      },
-      {
-        value: "Rhondda Cynon Taf",
-        label: "Rhondda Cynon Taf",
-      },
-      {
-        value: "Swansea",
-        label: "Swansea",
-      },
-      {
-        value: "Torfaen",
-        label: "Torfaen",
-      },
-      {
-        value: "Vale of Glamorgan",
-        label: "Vale of Glamorgan",
-      },
-      {
-        value: "Wrexham",
-        label: "Wrexham",
-      },
-    ],
+    options: councilArray,
   },
 };
