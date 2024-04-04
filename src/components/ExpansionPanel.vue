44ea0cfc2fea8580c94906e03eff4b5951787077
Zoe Curry <zoe.curry@wra.gov.wales>
2024-04-04 14:39:21 +0100

    ✨ new feature: expansion panels

---
+++ src/components/ExpansionPanel.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <button
+      :aria-controls="'expansion-content-' + ariaTitle"
+      :id="'expansion-control-' + ariaTitle"
+      @click="togglePanel"
+      :style="style"
+    >
+      {{ title }}
+      <span class="icon-wrapper">
+        <span
+          class="panel-icon"
+          :class="{
+            'show-panel-icon': showPanel,
+            'hide-panel-icon': !showPanel
+          }"
+        ></span>
+      </span>
+    </button>
+    <transition name="slide-fade">
+      <div
+        class="panel-content"
+        :aria-hidden="!showPanel"
+        :id="'content-' + ariaTitle"
+        v-if="showPanel"
+      >
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+
+export default {
+  name: "wra-expansion-panel",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    ariaTitle: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String
+    }
+  },
+  setup(props) {
+    props = reactive(props);
+    return {
+      style: computed(() => ({
+        color: props.color
+      }))
+    };
+  },
+  data() {
+    return {
+      showPanel: false
+    };
+  },
+  methods: {
+    togglePanel() {
+      this.showPanel = !this.showPanel;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.panel-icon {
+  content: " ";
+  display: inline-block;
+  border: solid #1f1f1f;
+  border-width: 3px 3px 0px 0px;
+  padding: 4px;
+  vertical-align: middle;
+  transition: transform 0.2s ease-out;
+}
+
+.icon-wrapper {
+  margin-left: 8px;
+}
+
+.show-panel-icon {
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+  margin-top: 3px;
+}
+
+.hide-panel-icon {
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+  margin-top: -5px;
+}
+
+button {
+  font-size: 18px;
+  font-weight: bold;
+  padding: 15px 20px;
+  background-color: transparent;
+  color: #2a225b;
+  border: none;
+  border-left: 2px solid #b9b9b9;
+  cursor: pointer;
+  width: 100%;
+  text-align: left;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+button:focus {
+  color: #1f1f1f !important;
+  background-color: #ffd530 !important;
+  outline: 2px solid #1f1f1f;
+  outline-offset: -2px;
+  border-color: #ffd530;
+}
+
+button:focus:hover {
+  opacity: 1;
+}
+
+.panel-content {
+  border-left: 2px solid #b9b9b9;
+  background: transparent;
+  padding: 15px 20px;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(-20px);
+  opacity: 0;
+}
+</style>
