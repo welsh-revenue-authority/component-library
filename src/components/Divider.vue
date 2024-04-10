2dfdb36e2e5366f6f41cecb713d3335d0837cd00
Jake <jake.real@wra.gov.wales>
2024-04-10 11:49:12 +0100

    ✨ new feature: divider, divider story

---
+++ src/components/Divider.vue
@@ -0,0 +1,27 @@
+<template>
+  <hr
+    role="presentation"
+    :style="{ borderTopWidth: thickness + 'px', borderColor: color }"
+  />
+</template>
+
+<script>
+export default {
+  name: "wra-divider",
+  props: {
+    thickness: {
+      type: Number,
+      default: 4,
+      validator(value) {
+        return value >= 0;
+      },
+    },
+    color: {
+      type: String,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/stories/Divider.stories.js
@@ -0,0 +1,16 @@
+import Divider from "../components/Divider.vue";
+
+export default {
+  title: "All/Divider",
+  component: Divider,
+  tags: ["autodocs"],
+  argTypes: {
+    color: {
+      control: "color",
+    },
+  },
+};
+
+export const Default = {
+  args: {},
+};
