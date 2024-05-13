398dd4a211b75f6c1615da4aa408f9d3b38153f7
pawlowski-d <107547150+pawlowski-d@users.noreply.github.com>
2024-05-13 12:23:07 +0100

    💄 data table: loading animation

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -35,7 +35,9 @@
 
       <tbody v-if="loading == true">
         <tr>
-          <td class="text-center" :colspan="headers.length">Loading...</td>
+          <td class="text-center" :colspan="headers.length">
+            <span class="loader"></span>
+          </td>
         </tr>
       </tbody>
 
@@ -236,5 +238,25 @@ th:hover .sort-icons {
 
 .disable-button {
   opacity: 0.3;
+}
+
+.loader {
+  width: 40px;
+  height: 40px;
+  border: 5px solid grey;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
