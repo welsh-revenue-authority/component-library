7c950e0d6cecc7307984360be06165f4c5747b11
Zoe Curry <zoe.curry@wra.gov.wales>
2024-06-05 14:55:53 +0100

    📝 update docs: simple table source code boxes

--- src/stories/SimpleTable.stories.js
+++ src/stories/SimpleTable.stories.js
@@ -44,7 +44,37 @@ export const Default = {
         </tr>
       </tbody>
     </wra-simple-table>`
-  })
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: `
+<wra-simple-table caption="Table caption">
+  <thead>
+    <tr>
+      <th>Date</th>
+      <th>Amount</th> 
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>First 6 weeks</td>
+      <td>£109.80 per week</td>
+    </tr>
+    <tr>
+      <td>Next 33 weeks</td>
+      <td>£109.80 per week</td>
+    </tr>
+    <tr>
+      <td>Total estimated pay</td>
+      <td>£4,282.20</td>
+    </tr>
+  </tbody>
+</wra-simple-table>
+        `
+      }
+    }
+  }
 };
 
 export const InheritBackground = {
@@ -80,5 +110,35 @@ export const InheritBackground = {
         </tr>
       </tbody>
     </wra-simple-table>`
-  })
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: `
+<wra-simple-table caption="Table caption" inherit-background>
+  <thead>
+    <tr>
+      <th>Date</th>
+      <th>Amount</th> 
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>First 6 weeks</td>
+      <td>£109.80 per week</td>
+    </tr>
+    <tr>
+      <td>Next 33 weeks</td>
+      <td>£109.80 per week</td>
+    </tr>
+    <tr>
+      <td>Total estimated pay</td>
+      <td>£4,282.20</td>
+    </tr>
+  </tbody>
+</wra-simple-table>
+        `
+      }
+    }
+  }
 };
