043ca6517c3ca782d2bfe0fbafee619a3e446a2d
Zoe Curry <zoe.curry@wra.gov.wales>
2024-06-06 09:02:33 +0100

    📝 update docs: update main.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,62 @@
 import "../src/styles/global.css";
 
-import ValidationTooltip from "./components/ValidationTooltip.vue";
+//Alphabetical order
+import AppBar from "./components/AppBar.vue";
+import Autocomplete from "./components/Autocomplete.vue";
+import Banner from "./components/Banner.vue";
+import Breadcrumbs from "./components/Breadcrumbs.vue";
+import Button from "./components/Button.vue";
 import Checkbox from "./components/Checkbox.vue";
 import CheckboxGroup from "./components/CheckboxGroup.vue";
 import ContentSectionTitle from "./components/ContentSectionTitle.vue";
-import Radio from "./components/Radio.vue";
-import RadioGroup from "./components/RadioGroup.vue";
-import Select from "./components/Select.vue";
-import Subheader from "./components/Subheader.vue";
-import TextArea from "./components/TextArea.vue";
-import Header from "./components/Header.vue";
-import Button from "./components/Button.vue";
 import DateInput from "./components/DateInput.vue";
+import Divider from "./components/Divider.vue";
+import ExpansionPanel from "./components/ExpansionPanel.vue";
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
+import Icon from "./components/Icon.vue";
 import NumberInput from "./components/NumberInput.vue";
 import PriceInput from "./components/PriceInput.vue";
-import TextInput from "./components/TextInput.vue";
-import Banner from "./components/Banner.vue";
-import Snackbar from "./components/Snackbar.vue";
-import ExpansionPanel from "./components/ExpansionPanel.vue";
-import Divider from "./components/Divider.vue";
-import Breadcrumbs from "./components/Breadcrumbs.vue";
-import Autocomplete from "./components/Autocomplete.vue";
+import RadioGroup from "./components/RadioGroup.vue";
+import Select from "./components/Select.vue";
+import ServiceLogo from "./components/ServiceLogo.vue";
 import SimpleTable from "./components/SimpleTable.vue";
-import Footer from "./components/Footer.vue";
+import Snackbar from "./components/Snackbar.vue";
+import Subheader from "./components/Subheader.vue";
+import Table from "./components/Table.vue";
+import Tabs from "./components/Tabs.vue";
+import TextArea from "./components/TextArea.vue";
+import TextInput from "./components/TextInput.vue";
+import ValidationTooltip from "./components/ValidationTooltip.vue";
 
 const components = {
-  ValidationTooltip,
+  AppBar,
+  Autocomplete,
+  Banner,
+  Breadcrumbs,
+  Button,
   Checkbox,
   CheckboxGroup,
   ContentSectionTitle,
-  Radio,
-  RadioGroup,
-  Select,
-  Subheader,
-  TextArea,
-  Header,
-  Button,
   DateInput,
+  Divider,
+  ExpansionPanel,
+  Footer,
+  Header,
+  Icon,
   NumberInput,
   PriceInput,
-  TextInput,
-  Banner,
-  Snackbar,
-  ExpansionPanel,
-  Divider,
-  Breadcrumbs,
-  Autocomplete,
+  RadioGroup,
+  Select,
+  ServiceLogo,
   SimpleTable,
-  Footer
+  Snackbar,
+  Subheader,
+  Table,
+  Tabs,
+  TextArea,
+  TextInput,
+  ValidationTooltip
 };
 
 export default (app) => {
