29c1be7cba1fa97470484da841cb42d4daa64d0d
Zoe Curry <zoe.curry@wra.gov.wales>
2024-06-13 13:16:46 +0100

    ✨ new feature: white button

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -15,7 +15,7 @@ export default {
       control: {
         type: "radio"
       },
-      options: ["wra-revenue", "wra-blue", "wra-red", "wra-green"]
+      options: ["wra-revenue", "wra-blue", "wra-red", "wra-green", "wra-white"]
     },
     outlined: {
       control: "boolean"
