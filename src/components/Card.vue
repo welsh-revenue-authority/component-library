56bcacaac20107757f511e5ed73b0c8ad4d011eb
Zoe Curry <zoe.curry@wra.gov.wales>
2024-07-23 08:19:55 +0100

    ✨ new feature: card

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "wra-card"
+};
+</script>
+
+<style scoped>
+.card {
+  background-color: #f1f1f1;
+  padding: 20px 24px;
+}
+</style>
