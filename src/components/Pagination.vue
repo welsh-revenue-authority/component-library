6f0ebf3f72443c4704f1b604ebbf5be89e99dd31
Jake <Jake.Real@wra.gov.wales>
2024-09-10 11:10:50 +0100

    ✨ new feature: pagination component and story

---
+++ src/components/Pagination.vue
@@ -0,0 +1,244 @@
+<template>
+  <div class="pagination">
+    <button
+      class="pagination-button"
+      @click="decPage()"
+      @keyup.enter="decPage()"
+    >
+      <slot name="previous">
+        <!-- Standard content -->
+        <span class="pagination-icon previous-icon"></span>
+        <span>{{ previousLabel }}</span>
+      </slot>
+    </button>
+
+    <!-- Skip to start buttons -->
+    <template v-if="skipStart">
+      <button
+        v-if="skipStart"
+        @click="changePage(startIndex)"
+        class="pagination-number"
+      >
+        {{ startIndex }}
+      </button>
+      <slot name="divider">
+        <span class="pagination-divider">...</span>
+      </slot>
+    </template>
+
+    <button
+      v-for="page in viewablePages"
+      @click="changePage(page)"
+      :ref="page"
+      class="pagination-number"
+      :class="currentPage === page && 'pagination-number--selected'"
+    >
+      {{ page }}
+    </button>
+
+    <template v-if="skipEnd">
+      <span class="pagination-divider">...</span>
+      <button @click="changePage(totalPages)" class="pagination-number">
+        {{ totalPages }}
+      </button>
+    </template>
+
+    <button
+      class="pagination-button"
+      @click="incPage()"
+      @keyup.enter="incPage()"
+    >
+      <slot name="next">
+        <span>{{ nextLabel }}</span>
+        <span class="pagination-icon next-icon"></span>
+      </slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "wra-pagination",
+  data: () => ({
+    currentPage: 0
+  }),
+  props: {
+    modelValue: {
+      type: Number,
+      required: true,
+      default: 1
+    },
+    totalPages: {
+      type: Number,
+      required: true
+    },
+    length: {
+      type: Number,
+      default: 5
+    },
+    skip: {
+      type: Boolean,
+      default: true
+    },
+    startIndex: {
+      type: Number,
+      default: 1
+    },
+    previousLabel: {
+      type: String,
+      default: "Previous"
+    },
+    nextLabel: {
+      type: String,
+      default: "Next"
+    }
+  },
+  methods: {
+    incPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+        this.$emit("update:currentPage", this.currentPage);
+      }
+    },
+    decPage() {
+      if (this.currentPage >= this.startIndex) {
+        this.currentPage--;
+        this.$emit("update:currentPage", this.currentPage);
+      }
+    },
+    changePage(page) {
+      this.currentPage = page;
+      this.$emit("update:currentPage", this.currentPage);
+      this.$nextTick(() => {
+        if (!this.$refs?.[page]?.[0]) {
+          console.log("No ref found for page", page);
+        }
+        this.$refs[page][0].focus();
+      });
+    }
+  },
+  emits: ["update:currentPage"],
+  computed: {
+    pageArray() {
+      return Array.from(
+        { length: this.totalPages },
+        (_, i) => i + this.startIndex
+      );
+    },
+    viewablePages() {
+      return this.pageArray.slice(
+        Math.max(0, this.currentPage - Math.ceil(this.length / 2)),
+        Math.min(
+          this.totalPages,
+          this.currentPage + Math.trunc(this.length / 2)
+        )
+      );
+    },
+    skipStart() {
+      let visibleRange = Math.ceil(this.length / 2) + this.startIndex;
+      return this.skip && this.currentPage >= visibleRange;
+    },
+    skipEnd() {
+      let visibleRange =
+        this.totalPages - Math.floor(this.length / 2) - this.startIndex;
+      return this.skip && this.currentPage <= visibleRange;
+    }
+  },
+  watch: {
+    modelValue(newValue) {
+      this.changePage(newValue);
+    }
+  },
+  created() {
+    this.currentPage = this.modelValue;
+  }
+};
+</script>
+
+<style scoped>
+.pagination-button {
+  background-color: transparent;
+  color: #0360a6;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+  font-size: 18px;
+  text-decoration: underline;
+}
+
+.pagination-button > *:first-child {
+  margin-right: 8px;
+}
+
+.pagination-button:hover,
+.pagination-number:hover {
+  text-decoration: none;
+  opacity: 0.8;
+  transition: all 0.3s ease;
+}
+
+.pagination-button:focus,
+.pagination-number:focus {
+  color: #1f1f1f;
+  background-color: #ffd530;
+  outline: 2px solid #1f1f1f;
+  outline-offset: 0px;
+  border-color: #ffd530;
+}
+
+.pagination-button:focus:hover,
+.pagination-number:focus:hover {
+  opacity: 1;
+}
+
+.pagination-number {
+  color: #2a2a2a;
+  padding: 8px;
+  font-weight: 700;
+  border: none;
+  font-size: 18px;
+  cursor: pointer;
+  width: 3rem;
+  text-align: center;
+}
+
+.pagination-number:hover {
+  color: #2a2a2a;
+  opacity: 0.8;
+}
+
+.pagination-number--selected {
+  background-color: #0360a6;
+  color: white;
+}
+
+.pagination-divider {
+  font-size: 18px;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.pagination-icon {
+  content: " ";
+  display: inline-block;
+  border: solid currentColor;
+  padding: 4px;
+  vertical-align: middle;
+  transform: rotate(-135deg);
+  -webkit-transform: rotate(-135deg);
+  margin-bottom: 3px;
+}
+
+.previous-icon {
+  border-width: 3px 3px 0px 0px;
+}
+
+.next-icon {
+  border-width: 0px 0px 3px 3px;
+}
+</style>
