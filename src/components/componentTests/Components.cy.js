2c1c3c6f1980a2550472f2fce11f02900e89b86b
LewisCilia <Lewis.Cilia@wra.gov.wales>
2024-10-01 11:23:46 +0100

    🧪 Component tests

---
+++ src/components/componentTests/Components.cy.js
@@ -0,0 +1,224 @@
+import AppBar from '../AppBar.vue'
+import Autocomplete from '../Autocomplete.vue'
+import Banner from '../Banner.vue'
+import Breadcrumbs from '../Breadcrumbs.vue'
+import Button from '../Button.vue'
+import Card from '../Card.vue'
+import Checkbox from '../Checkbox.vue'
+import CheckboxGroup from '../CheckboxGroup.vue'
+import ContentSectionTitle from '../ContentSectionTitle.vue'
+import CustomInput from '../CustomInput.vue'
+import DateInput from '../DateInput.vue'
+import Divider from '../Divider.vue'
+import ExpansionPanel from '../ExpansionPanel.vue'
+import Footer from '../Footer.vue'
+import Header from '../Header.vue'
+import Icon from '../Icon.vue'
+import NumberInput from '../NumberInput.vue'
+import Pagination from '../Pagination.vue'
+import PriceInput from '../PriceInput.vue'
+import Radio from '../Radio.vue'
+import RadioGroup from '../RadioGroup.vue'
+import Select from '../Select.vue'
+import ServiceLogo from '../ServiceLogo.vue'
+import SimpleTable from '../SimpleTable.vue'
+import Snackbar from '../Snackbar.vue'
+import Subheader from '../Subheader.vue'
+import Tab from '../Tab.vue'
+import Table from '../Table.vue'
+import Tabs from '../Tabs.vue'
+import TextArea from '../TextArea.vue'
+import TextInput from '../TextInput.vue'
+import ValidationTooltip from '../ValidationTooltip.vue'
+
+describe('AppBar', () => {
+    it('Renders', () => {
+        cy.mount(AppBar)
+    })
+})
+
+describe('Autocomplete', () => {
+    it('Renders', () => {
+        cy.mount(Autocomplete)
+    })
+})
+
+describe('Banner', () => {
+    it('Renders', () => {
+        cy.mount(Banner)
+    })
+})
+
+describe('Breadcrumbs', () => {
+    it('Renders', () => {
+        cy.mount(Breadcrumbs)
+    })
+})
+
+describe('Button', () => {
+    it('Renders', () => {
+        cy.mount(Button)
+    })
+})
+
+describe('Card', () => {
+    it('Renders', () => {
+        cy.mount(Card)
+    })
+})
+
+describe('Checkbox', () => {
+    it('Renders', () => {
+        cy.mount(Checkbox)
+    })
+})
+
+describe('CheckboxGroup', () => {
+    it('Renders', () => {
+        cy.mount(CheckboxGroup)
+    })
+})
+
+describe('ContentSectionTitle', () => {
+    it('Renders', () => {
+        cy.mount(ContentSectionTitle)
+    })
+})
+
+describe('CustomInput', () => {
+    it('Renders', () => {
+        cy.mount(CustomInput)
+    })
+})
+
+describe('DateInput', () => {
+    it('Renders', () => {
+        cy.mount(DateInput)
+    })
+})
+
+describe('Divider', () => {
+    it('Renders', () => {
+        cy.mount(Divider)
+    })
+})
+
+describe('ExpansionPanel', () => {
+    it('Renders', () => {
+        cy.mount(ExpansionPanel)
+    })
+})
+
+describe('Footer', () => {
+    it('Renders', () => {
+        cy.mount(Footer)
+    })
+})
+
+describe('Header', () => {
+    it('Renders', () => {
+        cy.mount(Header)
+    })
+})
+
+describe('Icon', () => {
+    it('Renders', () => {
+        cy.mount(Icon)
+    })
+})
+
+describe('NumberInput', () => {
+    it('Renders', () => {
+        cy.mount(NumberInput)
+    })
+})
+
+describe('Pagination', () => {
+    it('Renders', () => {
+        cy.mount(Pagination)
+    })
+})
+
+describe('PriceInput', () => {
+    it('Renders', () => {
+        cy.mount(PriceInput)
+    })
+})
+
+describe('Radio', () => {
+    it('Renders', () => {
+        cy.mount(Radio)
+    })
+})
+
+describe('RadioGroup', () => {
+    it('Renders', () => {
+        cy.mount(RadioGroup)
+    })
+})
+
+describe('Select', () => {
+    it('Renders', () => {
+        cy.mount(Select)
+    })
+})
+
+describe('ServiceLogo', () => {
+    it('Renders', () => {
+        cy.mount(ServiceLogo)
+    })
+})
+
+describe('SimpleTable', () => {
+    it('Renders', () => {
+        cy.mount(SimpleTable)
+    })
+})
+
+describe('Snackbar', () => {
+    it('Renders', () => {
+        cy.mount(Snackbar)
+    })
+})
+
+describe('Subheader', () => {
+    it('Renders', () => {
+        cy.mount(Subheader)
+    })
+})
+
+describe('Tab', () => {
+    it('Renders', () => {
+        cy.mount(Tab)
+    })
+})
+
+describe('Table', () => {
+    it('Renders', () => {
+        cy.mount(Table)
+    })
+})
+
+describe('Tabs', () => {
+    it('Renders', () => {
+        cy.mount(Tabs)
+    })
+})
+
+describe('TextArea', () => {
+    it('Renders', () => {
+        cy.mount(TextArea)
+    })
+})
+
+describe('TextInput', () => {
+    it('Renders', () => {
+        cy.mount(TextInput)
+    })
+})
+
+describe('ValidationTooltip', () => {
+    it('Renders', () => {
+        cy.mount(ValidationTooltip)
+    })
+})
