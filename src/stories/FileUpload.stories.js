d55a24710299f766da074f558fe058c276bde29d
Zoe Curry <zoe.curry@wra.gov.wales>
2024-10-02 14:00:04 +0100

    ✨ new feature: custom file upload

--- src/stories/FileUpload.stories.js
+++ src/stories/FileUpload.stories.js
@@ -3,19 +3,38 @@ import FileUpload from "../components/FileUpload.vue";
 export default {
   title: "Form Inputs & Controls/FileUpload",
   component: FileUpload,
-  tags: ["autodocs"]
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: {
+      control: {
+        type: "radio"
+      },
+      options: ["wra-revenue", "wra-blue", "wra-red", "wra-green", "wra-white"]
+    }
+  }
 };
 
 export const Default = {
   args: {
-    label: "Upload a file",
+    label: "File upload label",
+    buttonText: "Upload file",
     error: false
+  }
+};
+
+export const CustomButtonColour = {
+  args: {
+    label: "File upload label",
+    buttonText: "Upload file",
+    error: false,
+    backgroundColor: "wra-blue"
   }
 };
 
 export const Error = {
   args: {
-    label: "Upload a file",
+    label: "File upload label",
+    buttonText: "Upload file",
     error: true,
     errorMessage: "File is too large"
   }
