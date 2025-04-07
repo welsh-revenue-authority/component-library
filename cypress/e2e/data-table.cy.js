4dcd4a0bec22c2eb97c6ae0494402152af0d0079
LewisCilia <Lewis.Cilia@wra.gov.wales>
2025-04-07 13:59:16 +0100

    ✅ add: AppBar Storybook x Cypress test

--- cypress/e2e/data-table.cy.js
+++ cypress/e2e/data-table.cy.js
@@ -1,12 +1,23 @@
 import DataTable from "../page-objects/data-table";
 
-describe.only("DataTable", () => {
+describe("DataTable", () => {
   const dt = new DataTable();
 
-  it("Renders", () => {
-    cy.fixture("ComponentFixtures").then((f) => {
-      cy.visit("/iframe.html?id=data-display-datatable--default");
-      cy.get(dt.topRowName).should("have.text", "Artichoke");
+  beforeEach("Storybook load", () => {
+    cy.visit("/iframe.html?id=data-display-datatable--default");
+    cy.log("Visiting the data table component story");
+  });
+
+  it("Data Table - Default", () => {
+    cy.fixture("component-fixtures").then((f) => {
+      cy.get(dt.topRowName).should("have.text", f.vegArtichoke);
+      cy.log("Assert top row name vegetable name is Artichoke");
+
+      cy.get(dt.vegetableNameInput).type(f.vegBroccoli);
+      cy.log("Type in a valid vegetable name");
+
+      cy.get(dt.topRowName).should("have.text", f.vegBroccoli);
+      cy.log("Assert top row name vegetable name is Broccoli");
     });
   });
 });
--- cypress/page-objects/data-table.js
+++ cypress/page-objects/data-table.js
@@ -1,5 +1,6 @@
 class DataTable {
   topRowName = "tbody:nth-child(3) > tr > td:nth-child(1)";
+  vegetableNameInput = "div > table > thead > tr > th:nth-child(1) > input";
 }
 
 export default DataTable;
