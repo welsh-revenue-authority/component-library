fd2ac6580d86056d87770cec493ff133581c3bac
LewisCilia <Lewis.Cilia@wra.gov.wales>
2025-04-14 15:51:29 +0100

    ✅ add: banner & breadcrumb tests

---
+++ cypress/e2e/breadcrumbs.cy.js
@@ -0,0 +1,58 @@
+import Breadcrumbs from "../page-objects/breadcrumbs.js";
+
+describe("Breadcrumbs", () => {
+  const b = new Breadcrumbs();
+
+  it("Breadcrumbs - Default", () => {
+    cy.fixture("component-fixtures").then((f) => {
+      cy.visit("/iframe.html?id=navigation-breadcrumbs--default");
+      cy.log("Visiting the breadcrumbs component story");
+
+      cy.get(b.activePage).should("be.visible");
+      cy.log("Assert active page is visible and has correct class");
+
+      cy.get(b.inactivePageHyperlink)
+        .should("be.visible")
+        .should("have.attr", "href");
+      cy.log("Assert inactive page is visible and has href attribute");
+
+      cy.get(b.breadcrumbDivider)
+        .should("be.visible")
+        .should("have.class", "wra-breadcrumbs-chevron");
+      cy.log("Assert breadcrumb divider is visible and has correct class");
+    });
+  });
+
+  it("Breadcrumbs - Replace Divider", () => {
+    cy.fixture("component-fixtures").then((f) => {
+      cy.visit("/iframe.html?id=navigation-breadcrumbs--replace-divider");
+      cy.log("Visiting the breadcrumbs component story");
+
+      cy.get(b.activePage).should("be.visible");
+      cy.log("Assert active page is visible and has correct class");
+
+      cy.get(b.inactivePageHyperlink)
+        .should("be.visible")
+        .should("have.attr", "href");
+      cy.log("Assert inactive page is visible and has href attribute");
+    });
+  });
+
+  it("Breadcrumbs - Replace Content", () => {
+    cy.fixture("component-fixtures").then((f) => {
+      cy.visit("/iframe.html?id=navigation-breadcrumbs--replace-content");
+      cy.log("Visiting the breadcrumbs component story");
+
+      cy.get(b.activePage).should("be.visible");
+      cy.log("Assert active page is visible and has correct class");
+
+      cy.get(b.inactivePage).should("be.visible");
+      cy.log("Assert inactive page is visible");
+
+      cy.get(b.breadcrumbDivider)
+        .should("be.visible")
+        .should("have.class", "wra-breadcrumbs-chevron");
+      cy.log("Assert breadcrumb divider is visible and has correct class");
+    });
+  });
+});
---
+++ cypress/page-objects/banner.js
@@ -0,0 +1,8 @@
+class Banner {
+  acceptCookiesButton = "div > div.wra-banner-actions > button:nth-child(1)";
+  bannerHyperlink = "div > div > a";
+  changeCookiesSettingsButton =
+    "div > div.wra-banner-actions > button:nth-child(2)";
+}
+
+export default Banner;
