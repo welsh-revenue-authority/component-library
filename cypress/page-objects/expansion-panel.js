11419ed92d93a3a415e0568bbbb2d53adb7ee2aa
LewisCilia <Lewis.Cilia@wra.gov.wales>
2025-05-29 14:43:05 +0100

    ✅ update: expansion panel title slot

--- cypress/page-objects/expansion-panel.js
+++ cypress/page-objects/expansion-panel.js
@@ -1,6 +1,13 @@
 class ExpansionPanel {
   expansionControl = "#expansion-control-expansion-panel-title";
+  expansionControlTitleSlot = "#expansion-control-expansion-panel-title-slot";
   expansionText = "#content-expansion-panel-title > div > div > p:nth-child(1)";
+  expansionSlot1 =
+    "#expansion-control-expansion-panel-title-slot > div > span:nth-child(1)";
+  expansionSlot2 =
+    "#expansion-control-expansion-panel-title-slot > div > span:nth-child(2)";
+  expansionSlot3 =
+    "#expansion-control-expansion-panel-title-slot > div > span:nth-child(3)";
 }
 
 export default ExpansionPanel;
