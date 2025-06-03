b83814cb36bc0c1974defc2faba4cbaa58b1e62a
LewisCilia <Lewis.Cilia@wra.gov.wales>
2025-06-03 15:41:32 +0100

    🧪 update: up to inset

--- cypress/e2e/divider.cy.js
+++ cypress/e2e/divider.cy.js
@@ -8,8 +8,40 @@ describe("Divider", () => {
       cy.visit("/iframe.html?id=containment-divider--default");
       cy.log("Visiting the Divider component story");
 
-      cy.get(d.divider).should("be.visible");
-      cy.log("Asserted that the divider is visible");
+      cy.get(d.divider)
+        .should("be.visible")
+        .should("have.css", "border", "2.4px solid rgb(229, 229, 229)");
+      cy.log(
+        "Asserted that the divider is visible and has the correct border style"
+      );
+    });
+  });
+
+  it("Divider - Large Divider", () => {
+    cy.fixture("component-fixtures").then((f) => {
+      cy.visit("/iframe.html?id=containment-divider--large-divider");
+      cy.log("Visiting the Divider component story");
+
+      cy.get(d.divider)
+        .should("be.visible")
+        .should("have.css", "border", "4px solid rgb(229, 229, 229)");
+      cy.log(
+        "Asserted that the divider is visible and has the correct border style"
+      );
+    });
+  });
+
+  it("Divider - Small Divider", () => {
+    cy.fixture("component-fixtures").then((f) => {
+      cy.visit("/iframe.html?id=containment-divider--small-divider");
+      cy.log("Visiting the Divider component story");
+
+      cy.get(d.divider)
+        .should("be.visible")
+        .should("have.css", "border", "1.6px solid rgb(229, 229, 229)");
+      cy.log(
+        "Asserted that the divider is visible and has the correct border style"
+      );
     });
   });
 });
