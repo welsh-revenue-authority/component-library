b5514dffbb8e8c3b5fde87f7b5e91728eccc0ec1
Zoe Curry <zoe.curry@wra.gov.wales>
2025-06-05 15:22:16 +0100

    📝 update docs: custom icon docs

--- src/stories/Icon.stories.js
+++ src/stories/Icon.stories.js
@@ -4,7 +4,6 @@ import * as mdiIcons from "@mdi/js";
 export default {
   title: "Images & Icons/Icon",
   component: WraIcon,
-  tags: ["autodocs"],
   argTypes: {
     icon: {
       control: {
@@ -36,15 +35,6 @@ export default {
   }),
   parameters: {
     docs: {
-      description: {
-        component: `
-Icon that displays [Pictogrammers Material Design Icons](https://pictogrammers.com/library/mdi/)
-
-1. First, import the icon at the top of your script section
-2. Then, add it to your component's data
-3. Finally, use it in your template by passing it as a prop to the wra-icon component
-        `
-      },
       source: {
         code: `
 <template>
