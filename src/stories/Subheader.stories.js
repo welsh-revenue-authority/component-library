a86715bd4ef36caa41444af84b21693dc53c0460
Zoe Curry <zoe.curry@wra.gov.wales>
2025-06-17 07:39:22 +0100

    📝 update docs: subheader docs

--- src/stories/Subheader.stories.js
+++ src/stories/Subheader.stories.js
@@ -10,6 +10,14 @@ export default {
   argTypes: {
     color: {
       control: "color"
+    },
+    size: {
+      control: {
+        type: "select"
+      },
+      options: ["h1", "h2", "h3", "h4", "h5", "h6"],
+      description:
+        "The size of the header. This will apply the appropriate HTML tag and styling."
     }
   }
 };
