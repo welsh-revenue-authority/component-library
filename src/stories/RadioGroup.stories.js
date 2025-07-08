553bc43e8d0162ec2bf7a7706e91ea8cf8962185
Zoe Curry <zoe.curry@wra.gov.wales>
2025-07-08 10:44:37 +0100

    📝 update docs: radio button info text

--- src/stories/RadioGroup.stories.js
+++ src/stories/RadioGroup.stories.js
@@ -27,3 +27,36 @@ export const Default = {
     }
   }
 };
+
+export const InfoText = {
+  args: {
+    groupName: "groupName",
+    options: [
+      { value: "OptionOne", label: "Option 1", info: "This is option 1" },
+      { value: "OptionTwo", label: "Option 2", info: "This is option 2" }
+    ]
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `
+<wra-radio-group
+  group-name="groupName"
+  :options="options"
+></wra-radio-group>
+
+<script>
+export default {
+  data: () => ({
+    options: [
+      { value: "OptionOne", label: "Option 1", info: "This is option 1" },
+      { value: "OptionTwo", label: "Option 2", info: "This is option 2" }
+    ]
+  })
+}
+</script>
+        `
+      }
+    }
+  }
+};
