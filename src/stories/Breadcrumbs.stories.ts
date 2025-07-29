b2ae41bc19583046d43da87d5585625141a1b8bb
Zoe Curry <zoe.curry@wra.gov.wales>
2025-07-29 15:42:01 +0100

    ♻️ refactor code: breadcrumbs

---
+++ src/stories/Breadcrumbs.stories.ts
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import Breadcrumbs from "../components/Breadcrumbs.vue";
+
+const meta: Meta<typeof Breadcrumbs> = {
+  title: "Navigation/Breadcrumbs",
+  component: Breadcrumbs,
+  tags: ["autodocs"],
+  argTypes: {}
+};
+export default meta;
+
+type BreadcrumbsProps = InstanceType<typeof Breadcrumbs>["$props"];
+type Story = StoryObj<BreadcrumbsProps>;
+
+export const Default: Story = {
+  args: {
+    items: [
+      { title: "Dashboard", disabled: false, href: "." },
+      { title: "Link 1", disabled: false, href: "." },
+      { title: "Link 2", disabled: true, href: "." }
+    ]
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `
+<wra-breadcrumbs :items="items" />
+        `
+      }
+    }
+  }
+};
+
+export const replaceDivider: Story = {
+  args: {
+    items: [
+      { title: "Dashboard", disabled: false, href: "." },
+      { title: "Link 1", disabled: false, href: "." },
+      { title: "Link 2", disabled: true, href: "." }
+    ]
+  },
+  render: (args) => ({
+    components: { Breadcrumbs },
+    setup() {
+      return { args };
+    },
+    template: `
+      <Breadcrumbs v-bind="args">
+        <template #divider> - </template>
+      </Breadcrumbs>
+    `
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: `
+<wra-breadcrumbs :items="items">
+  <template #divider> - </template>
+</wra-breadcrumbs>
+        `
+      }
+    }
+  }
+};
+
+export const replaceContent: Story = {
+  args: {
+    items: [
+      { title: "Dashboard", disabled: false, href: "." },
+      { title: "Link 1", disabled: false, href: "." },
+      { title: "Link 2", disabled: true, href: "." }
+    ]
+  },
+  render: (args) => ({
+    components: { Breadcrumbs },
+    setup() {
+      return { args };
+    },
+    template: `
+      <Breadcrumbs v-bind="args">
+        <template #default> Link </template>
+      </Breadcrumbs>
+    `
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: `
+<wra-breadcrumbs :items="items">
+  <template #default> Link </template>
+</wra-breadcrumbs>
+        `
+      }
+    }
+  }
+};
