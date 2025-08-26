3af506b0b7a5d808c9cbed21619683f08876ee72
Zoe Curry <zoe.curry@wra.gov.wales>
2025-08-26 10:16:28 +0100

    🐛 bug fix: typescript will now recognize .vue imports

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,9 @@ declare module "*.svg" {
   const src: string;
   export default src;
 }
+
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
