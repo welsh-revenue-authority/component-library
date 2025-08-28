bbc077e8ef90d3041800dc12adabb1fdb946df47
Zoe Curry <zoe.curry@wra.gov.wales>
2025-08-28 10:32:01 +0100

    ✅ update test: select

--- cypress/page-objects/select.js
+++ cypress/page-objects/select.js
@@ -1,6 +1,9 @@
 class Select {
   option1 = "#storybook-root > div > select > option:nth-child(2)";
   option2 = "#storybook-root > div > select > option:nth-child(3)";
+  option3 = "#storybook-root > div > select > option:nth-child(4)";
+  option4 = "#storybook-root > div > select > option:nth-child(5)";
+  option5 = "#storybook-root > div > select > option:nth-child(6)";
   select = "#storybook-root > div > select";
 }
 
