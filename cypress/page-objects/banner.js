c5814692733cc8fa9bad6c65ae6868993eb48f43
Zoe Cilia <zoe.curry@wra.gov.wales>
2026-04-27 14:51:34 +0100

    ✅ update tests

--- cypress/page-objects/banner.js
+++ cypress/page-objects/banner.js
@@ -3,7 +3,9 @@ class Banner {
     "#storybook-root > div > div.wra-banner-actions > button:nth-child(1)";
   banner = "#storybook-root > div";
   bannerCloseButton = "#storybook-root > div > button";
-  bannerHyperlink = "#storybook-root > div > div > a";
+  bannerHyperlink = "#storybook-root > div > div.wra-banner-body > span > a";
+  bannerStatusTag =
+    "#storybook-root > div > div.wra-banner-body > span.wra-status-tag";
   changeCookiesSettingsButton =
     "#storybook-root > div > div.wra-banner-actions > button:nth-child(2)";
 }
